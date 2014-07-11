662b76887115c12231534f578f58029ae22cc542
Ying Yi <517576002@qq.com>
2014-07-11 15:21:29 +0800

    Create MJYiDataSource.m

---
+++ MJRefreshExample/MJRefreshExample/MJRefresh/MJYiDataSource.m
@@ -0,0 +1,59 @@
+//
+//  MJYiDataSource.m
+//  MJRefreshExample
+//
+//  Created by Ying Yi on 14-7-11.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import "MJYiDataSource.h"
+
+@implementation MJYiDataSource
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.dsArray = [[NSMutableArray alloc]initWithCapacity:32];
+        
+        dataSourcePageInfoStruct stru;
+        stru.page = 0;
+        stru.totalPages = 0;
+        
+        self.pageStruct = stru;
+        
+        
+    }
+    return self;
+}
+
+-(void)reset
+{
+    [self setPageInfoWithPage:0 AndTotalPages:0];
+    [self.dsArray removeAllObjects];
+}
+
+
+- (void)setPageInfoWithPage:(NSInteger)page AndTotalPages:(NSInteger)totalPages
+{
+    dataSourcePageInfoStruct stru;
+    stru.page = page;
+    stru.totalPages = totalPages;
+    self.pageStruct = stru;
+}
+
+- (void)addPage
+{
+    [self setPageInfoWithPage:self.pageStruct.page+1 AndTotalPages:self.pageStruct.totalPages];
+}
+
+- (NSInteger)page
+{
+    return self.pageStruct.page;
+}
+
+- (NSInteger)totalPages
+{
+    return self.pageStruct.totalPages;
+}
+
+@end
