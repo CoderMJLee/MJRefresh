5a1a10da2075e82988bb4dcf114ea536155b4328
破船 <BeyondVincent@gmail.com>
2015-02-07 21:14:06 +0800

    Update MJRefreshConst.m
    修改刷新提示语。

--- MJRefreshExample/MJRefreshExample/MJRefresh/MJRefreshConst.m
+++ MJRefreshExample/MJRefreshExample/MJRefresh/MJRefreshConst.m
@@ -14,11 +14,11 @@ const CGFloat MJRefreshSlowAnimationDuration = 0.4;
 
 NSString *const MJRefreshFooterPullToRefresh = @"上拉可以加载更多数据";
 NSString *const MJRefreshFooterReleaseToRefresh = @"松开立即加载更多数据";
-NSString *const MJRefreshFooterRefreshing = @"MJ哥正在帮你加载数据...";
+NSString *const MJRefreshFooterRefreshing = @"正在加载数据...";
 
 NSString *const MJRefreshHeaderPullToRefresh = @"下拉可以刷新";
 NSString *const MJRefreshHeaderReleaseToRefresh = @"松开立即刷新";
-NSString *const MJRefreshHeaderRefreshing = @"MJ哥正在帮你刷新...";
+NSString *const MJRefreshHeaderRefreshing = @"正在刷新...";
 NSString *const MJRefreshHeaderTimeKey = @"MJRefreshHeaderView";
 
 NSString *const MJRefreshContentOffset = @"contentOffset";
