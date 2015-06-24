5a742d5543f8315d8d3ecc03f4ca6ce99403a23b
MJLee <199109106@qq.com>
2015-06-24 19:31:42 +0800

    Fix double footer beginRefreshing
    Fix double footer beginRefreshing

--- MJRefresh/Base/MJRefreshAutoFooter.m
+++ MJRefresh/Base/MJRefreshAutoFooter.m
@@ -58,6 +58,11 @@
     if (_scrollView.mj_insetT + _scrollView.mj_contentH > _scrollView.mj_h) { // 内容超过一个屏幕
         // 这里的_scrollView.mj_contentH替换掉self.mj_y更为合理
         if (_scrollView.mj_offsetY > _scrollView.mj_contentH - _scrollView.mj_h + self.mj_h * self.appearencePercentTriggerAutoRefresh + _scrollView.mj_insetB - self.mj_h) {
+            // 防止手松开时连续调用
+            CGPoint old = [change[@"old"] CGPointValue];
+            CGPoint new = [change[@"new"] CGPointValue];
+            if (new.y <= old.y) return;
+            
             // 当底部刷新控件完全出现时，才刷新
             [self beginRefreshing];
         }
