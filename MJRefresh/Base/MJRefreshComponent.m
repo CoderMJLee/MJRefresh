20fbab52d735509c6f7bc71dac336eab3ee90e4b
lisi <lisi_1987@foxmail.com>
2016-05-28 10:10:27 +0800

    修改获取framework NSBundle 的方式

--- MJRefresh/Base/MJRefreshComponent.m
+++ MJRefresh/Base/MJRefreshComponent.m
@@ -142,20 +142,11 @@
     static NSBundle *bundle = nil;
     if (bundle == nil)
     {
-        NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"MJRefresh" ofType:@"bundle"];
         
+        bundle = [NSBundle bundleForClass:self.class];
+        NSString *bundlePath = [bundle pathForResource:@"MJRefresh" ofType:@"bundle"];
         bundle = [NSBundle bundleWithPath:bundlePath];
-        NSString *language = [[NSLocale preferredLanguages] count]? [NSLocale preferredLanguages][0]: @"en";
-        if (![[bundle localizations] containsObject:language])
-        {
-            language = [language componentsSeparatedByString:@"-"][0];
-        }
-        if ([[bundle localizations] containsObject:language])
-        {
-            bundlePath = [bundle pathForResource:language ofType:@"lproj"];
-        }
-
-        bundle = [NSBundle bundleWithPath:bundlePath] ?: [NSBundle mainBundle];
+        
     }
     defaultString = [bundle localizedStringForKey:key value:defaultString table:nil];
     return [[NSBundle mainBundle] localizedStringForKey:key value:defaultString table:nil];
