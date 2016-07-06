0f27f5f9dfffb9fb59cf181c22921ae4f1dee93e
杨智晓 <yangzhixiao1989@foxmail.com>
2016-07-06 15:25:23 +0800

    适配swift
    对属性mj_header, mj_footer添加nullable标识

--- MJRefresh/UIScrollView+MJRefresh.h
+++ MJRefresh/UIScrollView+MJRefresh.h
@@ -14,10 +14,10 @@
 
 @interface UIScrollView (MJRefresh)
 /** 下拉刷新控件 */
-@property (strong, nonatomic) MJRefreshHeader *mj_header;
+@property (strong, nonatomic, nullable) MJRefreshHeader *mj_header;
 @property (strong, nonatomic) MJRefreshHeader *header MJRefreshDeprecated("使用mj_header");
 /** 上拉刷新控件 */
-@property (strong, nonatomic) MJRefreshFooter *mj_footer;
+@property (strong, nonatomic, nullable) MJRefreshFooter *mj_footer;
 @property (strong, nonatomic) MJRefreshFooter *footer MJRefreshDeprecated("使用mj_footer");
 
 #pragma mark - other
