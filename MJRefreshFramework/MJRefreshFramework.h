2f7b8ce8a977218e1e93a5a746dbcc4dc6370ce1
lisi <lisi_1987@foxmail.com>
2016-09-28 11:06:35 +0800

    支持Carthage

---
+++ MJRefreshFramework/MJRefreshFramework.h
@@ -0,0 +1,19 @@
+//
+//  MJRefreshFramework.h
+//  MJRefreshFramework
+//
+//  Created by Kevin on 9/28/16.
+//  Copyright © 2016 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MJRefreshFramework.
+FOUNDATION_EXPORT double MJRefreshFrameworkVersionNumber;
+
+//! Project version string for MJRefreshFramework.
+FOUNDATION_EXPORT const unsigned char MJRefreshFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJRefreshFramework/PublicHeader.h>
+
+
