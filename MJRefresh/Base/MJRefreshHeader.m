22e661aeb5965ae494c16c25fcf0ed87bea6cfbf
Vito <vvitozhang@gmail.com>
2017-06-27 15:15:09 +0800

    fix refresh header can’t hide when view is not showing

--- MJRefresh/Base/MJRefreshHeader.m
+++ MJRefresh/Base/MJRefreshHeader.m
@@ -54,8 +54,6 @@
     
     // 在刷新的refreshing状态
     if (self.state == MJRefreshStateRefreshing) {
-        if (self.window == nil) return;
-        
         // sectionheader停留解决
         CGFloat insetT = - self.scrollView.mj_offsetY > _scrollViewOriginalInset.top ? - self.scrollView.mj_offsetY : _scrollViewOriginalInset.top;
         insetT = insetT > self.mj_h + _scrollViewOriginalInset.top ? self.mj_h + _scrollViewOriginalInset.top : insetT;
