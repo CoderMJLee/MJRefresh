3f93f4fb5c240d3dbb0ec265a645d244d8ef2c2b
ungacy <ungacy@126.com>
2017-10-27 10:57:39 +0800

    add __kindof

--- MJRefresh/UIScrollView+MJRefresh.h
+++ MJRefresh/UIScrollView+MJRefresh.h
@@ -14,11 +14,11 @@
 
 @interface UIScrollView (MJRefresh)
 /** 下拉刷新控件 */
-@property (strong, nonatomic) MJRefreshHeader *mj_header;
-@property (strong, nonatomic) MJRefreshHeader *header MJRefreshDeprecated("使用mj_header");
+@property (strong, nonatomic) __kindof MJRefreshHeader *mj_header;
+@property (strong, nonatomic) __kindof MJRefreshHeader *header MJRefreshDeprecated("使用mj_header");
 /** 上拉刷新控件 */
-@property (strong, nonatomic) MJRefreshFooter *mj_footer;
-@property (strong, nonatomic) MJRefreshFooter *footer MJRefreshDeprecated("使用mj_footer");
+@property (strong, nonatomic) __kindof MJRefreshFooter *mj_footer;
+@property (strong, nonatomic) __kindof MJRefreshFooter *footer MJRefreshDeprecated("使用mj_footer");
 
 #pragma mark - other
 - (NSInteger)mj_totalDataCount;
