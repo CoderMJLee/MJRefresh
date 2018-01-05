71024196f360b9abc20609ad7e5753362aa21290
INCUBUS <409362416@qq.com>
2018-01-05 11:46:45 +0800

    应用内切换语言建议

--- MJRefresh/NSBundle+MJRefresh.m
+++ MJRefresh/NSBundle+MJRefresh.m
@@ -37,24 +37,31 @@
 + (NSString *)mj_localizedStringForKey:(NSString *)key value:(NSString *)value
 {
     static NSBundle *bundle = nil;
-    if (bundle == nil) {
-        // （iOS获取的语言字符串比较不稳定）目前框架只处理en、zh-Hans、zh-Hant三种情况，其他按照系统默认处理
-        NSString *language = [NSLocale preferredLanguages].firstObject;
-        if ([language hasPrefix:@"en"]) {
-            language = @"en";
-        } else if ([language hasPrefix:@"zh"]) {
-            if ([language rangeOfString:@"Hans"].location != NSNotFound) {
-                language = @"zh-Hans"; // 简体中文
-            } else { // zh-Hant\zh-HK\zh-TW
-                language = @"zh-Hant"; // 繁體中文
-            }
-        } else {
-            language = @"en";
+    //    if (bundle == nil) {
+    // （iOS获取的语言字符串比较不稳定）目前框架只处理en、zh-Hans、zh-Hant三种情况，其他按照系统默认处理
+    //        NSString *language = [NSLocale preferredLanguages].firstObject;
+    NSString *language = [[NSUserDefaults standardUserDefaults] objectForKey:@"appLanguage"];
+    /**
+     建议这里能够暴露出一个可共 应用内切换语言的接口
+     目前这种处理方式是我临时改的
+     谢谢.
+     by.李根
+     */
+    if ([language hasPrefix:@"en"]) {
+        language = @"en";
+    } else if ([language hasPrefix:@"zh"]) {
+        if ([language rangeOfString:@"Hans"].location != NSNotFound) {
+            language = @"zh-Hans"; // 简体中文
+        } else { // zh-Hant\zh-HK\zh-TW
+            language = @"zh-Hant"; // 繁體中文
         }
-        
-        // 从MJRefresh.bundle中查找资源
-        bundle = [NSBundle bundleWithPath:[[NSBundle mj_refreshBundle] pathForResource:language ofType:@"lproj"]];
+    } else {
+        language = @"en";
     }
+    
+    // 从MJRefresh.bundle中查找资源
+    bundle = [NSBundle bundleWithPath:[[NSBundle mj_refreshBundle] pathForResource:language ofType:@"lproj"]];
+    //    }
     value = [bundle localizedStringForKey:key value:value table:nil];
     return [[NSBundle mainBundle] localizedStringForKey:key value:value table:nil];
 }
