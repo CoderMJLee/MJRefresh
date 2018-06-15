cd14c2b353a87d061ea3b1ff9ea63c86d9a438ef
ingramwang2015 <ingramwang2015@gmail.com>
2018-06-15 11:25:56 +0800

    修复self respondsToSelector:@selector(adjustedContentInset)在+方法中的错误调用

--- MJRefresh/UIScrollView+MJExtension.m
+++ MJRefresh/UIScrollView+MJExtension.m
@@ -15,17 +15,20 @@
 
 @implementation UIScrollView (MJExtension)
 
-static BOOL gt_ios_11_;
-+ (void)load
+static BOOL _respondsAdjustedContentInsetMethod;
+- (BOOL)isRespondsAdjustedContentInset
 {
-    // 缓存判断值
-    gt_ios_11_ = ([[[UIDevice currentDevice] systemVersion] compare:@"11" options:NSNumericSearch] != NSOrderedAscending) && [self respondsToSelector:@selector(adjustedContentInset)];
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _respondsAdjustedContentInsetMethod = ([[[UIDevice currentDevice] systemVersion] compare:@"11" options:NSNumericSearch] != NSOrderedAscending) && [self respondsToSelector:@selector(adjustedContentInset)];
+    });
+    return _respondsAdjustedContentInsetMethod;
 }
 
 - (UIEdgeInsets)mj_inset
 {
 #ifdef __IPHONE_11_0
-    if (gt_ios_11_) {
+    if ([self isRespondsAdjustedContentInset]) {
         return self.adjustedContentInset;
     }
 #endif
@@ -37,7 +40,7 @@ static BOOL gt_ios_11_;
     UIEdgeInsets inset = self.contentInset;
     inset.top = mj_insetT;
 #ifdef __IPHONE_11_0
-    if (gt_ios_11_) {
+    if ([self isRespondsAdjustedContentInset]) {
         inset.top -= (self.adjustedContentInset.top - self.contentInset.top);
     }
 #endif
@@ -54,7 +57,7 @@ static BOOL gt_ios_11_;
     UIEdgeInsets inset = self.contentInset;
     inset.bottom = mj_insetB;
 #ifdef __IPHONE_11_0
-    if (gt_ios_11_) {
+    if ([self isRespondsAdjustedContentInset]) {
         inset.bottom -= (self.adjustedContentInset.bottom - self.contentInset.bottom);
     }
 #endif
@@ -71,7 +74,7 @@ static BOOL gt_ios_11_;
     UIEdgeInsets inset = self.contentInset;
     inset.left = mj_insetL;
 #ifdef __IPHONE_11_0
-    if (gt_ios_11_) {
+    if ([self isRespondsAdjustedContentInset]) {
         inset.left -= (self.adjustedContentInset.left - self.contentInset.left);
     }
 #endif
@@ -88,7 +91,7 @@ static BOOL gt_ios_11_;
     UIEdgeInsets inset = self.contentInset;
     inset.right = mj_insetR;
 #ifdef __IPHONE_11_0
-    if (gt_ios_11_) {
+    if ([self isRespondsAdjustedContentInset]) {
         inset.right -= (self.adjustedContentInset.right - self.contentInset.right);
     }
 #endif
