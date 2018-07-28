0ac4583e802251435c9220ede7a8a3e3a64fab6f
M了个J <199109106@qq.com>
2018-07-28 11:17:17 +0800

    Little change
    Little change

--- MJRefresh/UIScrollView+MJExtension.m
+++ MJRefresh/UIScrollView+MJExtension.m
@@ -15,22 +15,20 @@
 
 @implementation UIScrollView (MJExtension)
 
-static BOOL mj_respondsToAdjustedContentInset;
+static BOOL respondsToAdjustedContentInset_;
 
-- (BOOL)gt_respondsToAdjustedContentInset {
-    
++ (void)initialize
+{
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        mj_respondsToAdjustedContentInset = [self respondsToSelector:@selector(adjustedContentInset)];
+        respondsToAdjustedContentInset_ = [self respondsToSelector:@selector(adjustedContentInset)];
     });
-    
-    return mj_respondsToAdjustedContentInset;
 }
 
 - (UIEdgeInsets)mj_inset
 {
 #ifdef __IPHONE_11_0
-    if ([self gt_respondsToAdjustedContentInset]) {
+    if (respondsToAdjustedContentInset_) {
         return self.adjustedContentInset;
     }
 #endif
@@ -42,7 +40,7 @@ static BOOL mj_respondsToAdjustedContentInset;
     UIEdgeInsets inset = self.contentInset;
     inset.top = mj_insetT;
 #ifdef __IPHONE_11_0
-    if ([self gt_respondsToAdjustedContentInset]) {
+    if (respondsToAdjustedContentInset_) {
         inset.top -= (self.adjustedContentInset.top - self.contentInset.top);
     }
 #endif
@@ -59,7 +57,7 @@ static BOOL mj_respondsToAdjustedContentInset;
     UIEdgeInsets inset = self.contentInset;
     inset.bottom = mj_insetB;
 #ifdef __IPHONE_11_0
-    if ([self gt_respondsToAdjustedContentInset]) {
+    if (respondsToAdjustedContentInset_) {
         inset.bottom -= (self.adjustedContentInset.bottom - self.contentInset.bottom);
     }
 #endif
@@ -76,7 +74,7 @@ static BOOL mj_respondsToAdjustedContentInset;
     UIEdgeInsets inset = self.contentInset;
     inset.left = mj_insetL;
 #ifdef __IPHONE_11_0
-    if ([self gt_respondsToAdjustedContentInset]) {
+    if (respondsToAdjustedContentInset_) {
         inset.left -= (self.adjustedContentInset.left - self.contentInset.left);
     }
 #endif
@@ -93,7 +91,7 @@ static BOOL mj_respondsToAdjustedContentInset;
     UIEdgeInsets inset = self.contentInset;
     inset.right = mj_insetR;
 #ifdef __IPHONE_11_0
-    if ([self gt_respondsToAdjustedContentInset]) {
+    if (respondsToAdjustedContentInset_) {
         inset.right -= (self.adjustedContentInset.right - self.contentInset.right);
     }
 #endif
