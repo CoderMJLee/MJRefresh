ff81e2e4ae3a2daa77da79192b761686d1a3432d
daoseng33 <daoseng33@gmail.com>
2019-07-01 20:04:22 +0800

    設置觸動回饋默認為關閉

--- MJRefresh/Base/MJRefreshComponent.m
+++ MJRefresh/Base/MJRefreshComponent.m
@@ -25,7 +25,7 @@
         // 默认是普通状态
         self.state = MJRefreshStateIdle;
         // 默認開啟觸動回饋
-        self.hapticFeedbackEnabled = YES;
+        self.hapticFeedbackEnabled = NO;
     }
     return self;
 }
