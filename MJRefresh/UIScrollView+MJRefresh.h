25ebd074a2692e4c24d1f68ff774a508839f9795
Eric Wu <starring.wu@gmail.com>
2019-09-04 16:54:52 +0800

    对swift 友好

--- MJRefresh/UIScrollView+MJRefresh.h
+++ MJRefresh/UIScrollView+MJRefresh.h
@@ -14,11 +14,11 @@
 
 @interface UIScrollView (MJRefresh)
 /** 下拉刷新控件 */
-@property (strong, nonatomic) MJRefreshHeader *mj_header;
-@property (strong, nonatomic) MJRefreshHeader *header MJRefreshDeprecated("使用mj_header");
+@property (strong, nonatomic, nullable) MJRefreshHeader *mj_header;
+@property (strong, nonatomic, nullable) MJRefreshHeader *header MJRefreshDeprecated("使用mj_header");
 /** 上拉刷新控件 */
-@property (strong, nonatomic) MJRefreshFooter *mj_footer;
-@property (strong, nonatomic) MJRefreshFooter *footer MJRefreshDeprecated("使用mj_footer");
+@property (strong, nonatomic, nullable) MJRefreshFooter *mj_footer;
+@property (strong, nonatomic, nullable) MJRefreshFooter *footer MJRefreshDeprecated("使用mj_footer");
 
 #pragma mark - other
 - (NSInteger)mj_totalDataCount;
