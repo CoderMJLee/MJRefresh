927c50e09c115762f21d730408a1865600b0eedd
kelicheng <kelicheng131@gmail.com>
2020-03-30 18:01:35 +0800

    修复了，在设置header展示刷新文字的block后，第一次展示不会刷新文字的问题

--- MJRefresh/Custom/Header/MJRefreshStateHeader.m
+++ MJRefresh/Custom/Header/MJRefreshStateHeader.m
@@ -45,6 +45,12 @@
     return _lastUpdatedTimeLabel;
 }
 
+- (void)setLastUpdatedTimeText:(NSString * _Nonnull (^)(NSDate * _Nullable))lastUpdatedTimeText{
+    _lastUpdatedTimeText = lastUpdatedTimeText;
+    // 重新设置key（重新显示时间）
+    self.lastUpdatedTimeKey = self.lastUpdatedTimeKey;
+}
+
 #pragma mark - 公共方法
 - (void)setTitle:(NSString *)title forState:(MJRefreshState)state
 {
