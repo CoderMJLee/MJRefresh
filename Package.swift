2ce14b02d25c410f5c85e16551b21f506bbcefa7
Sereivoan Yong <sereivoanyong@gmail.com>
2021-02-11 10:05:40 +0700

    Increase swift-tools-version to 5.3

--- Package.swift
+++ Package.swift
@@ -1,16 +1,29 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
   name: "MJRefresh",
   platforms: [
-    .iOS(.v8),
+    .iOS(.v9),
   ],
   products: [
     .library(name: "MJRefresh", targets: ["MJRefresh"]),
   ],
   targets: [
-    .target(name: "MJRefresh", path: ".", sources: ["MJRefresh"], publicHeadersPath: "MJRefresh"),
-  ],
-  swiftLanguageVersions: [.v5]
+    .target(
+      name: "MJRefresh",
+      path: ".",
+      exclude: [
+        "Gif",
+        "MJRefreshExample",
+        "MJRefreshExampleTests",
+        "MJRefreshFramework",
+        "LICENSE",
+        "MJRefresh.podspec",
+        "README.md",
+      ],
+      sources: ["MJRefresh"],
+      publicHeadersPath: "MJRefresh"
+    ),
+  ]
 )
