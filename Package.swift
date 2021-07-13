d98919b3ee8129365239fdfc253427b75a2c6d0f
Frank <472730949@qq.com>
2021-07-13 14:59:01 +0800

    Add some missing link file Add missing headers for framework

--- Package.swift
+++ Package.swift
@@ -6,25 +6,20 @@ import PackageDescription
 let package = Package(
     name: "MJRefresh",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "MJRefresh",
-            targets: ["MJRefresh"]),
+        .library(name: "MJRefresh", targets: ["MJRefresh"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MJRefresh",
             dependencies: [],
             path: "MJRefresh",
             resources: [.process("MJRefresh.bundle")],
+            publicHeadersPath: "include",
             cSettings: [
-                .define("MJ_USEING_SPM"),//Define macro "MJ_USEING_SPM" to identify whether Cocoapods or SPM are being used
+                //Define macro "MJ_SPM" to identify whether Cocoapods or SPM are being used
+                .define("MJ_SPM"),
+                //Config header path
                 .headerSearchPath("."),
                 .headerSearchPath("Base"),
                 .headerSearchPath("Custom/Footer/Auto"),
