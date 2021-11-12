b7b247efe3e6a38c651143e9daaa57e3bc360e21
Frank <472730949@qq.com>
2021-11-12 16:47:00 +0800

    [Example] Fixed navigation bar problem in iOS15

--- MJRefreshExample/Classes/Other/AppDelegate.m
+++ MJRefreshExample/Classes/Other/AppDelegate.m
@@ -14,24 +14,11 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    [self setupNavigationBarAppearance];
+    
     return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
@@ -42,8 +29,27 @@
     });
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (void)setupNavigationBarAppearance {
+    // 1.取出设置主题的对象
+    UINavigationBar *navBar = [UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[UINavigationController.class]];
+    
+    // 2.设置导航栏的背景图片
+    UIImage *backgroundImage = [UIImage imageNamed:@"NavBar64"];
+    NSDictionary *titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+    
+    if (@available(iOS 13.0, *)) {
+        UINavigationBarAppearance *appearance = [UINavigationBarAppearance new];
+        appearance.backgroundImage = backgroundImage;
+        appearance.backgroundImageContentMode = UIViewContentModeScaleToFill;
+        appearance.titleTextAttributes = titleTextAttributes;
+        
+        navBar.scrollEdgeAppearance = appearance;
+        navBar.standardAppearance = appearance;
+    } else {
+        [[UINavigationBar appearance] setBackgroundImage:backgroundImage forBarMetrics:UIBarMetricsDefault];
+        // 3.标题
+        [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor]}];
+    }
 }
 
 @end
